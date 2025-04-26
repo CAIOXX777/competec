778680b33c0aa442e91ffcc2b600c5d9095022ff
CAIOXX <104595124+CAIOXX777@users.noreply.github.com>
2025-04-26 11:19:28 -0300

    Primeira aula.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	    
+	    Scanner entrada = new Scanner(System.in);
+	    
+	    char nome='C';
+	    int idade=15;
+	    boolean tomouCafeDaManha=true;
+	    int valorDaLaranja=7;
+	    int valorDaUva=10;
+	    int soma=valorDaUva+valorDaLaranja;
+	    
+	    System.out.print("Digite o valor da laranja:");
+	    int laranja=entrada.nextInt();
+	    
+	     System.out.print("Digite o valor da uva:");
+	    int uva=entrada.nextInt();
+	    
+		System.out.print("Resultado da soma ="+ (uva+ laranja) );
+		
+		
+		
 	}
 }
