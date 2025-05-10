66b1f9cc46ce4c69da473eed1532d0f21f540904
CAIOXX <104595124+CAIOXX777@users.noreply.github.com>
2025-05-10 11:55:53 -0300

    aula 01.java

--- aula-1/Main.java
+++ aula-1/Main.java
@@ -1,7 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+		char henry = 'h';
+		char caio = 'c';
+		System.out.println(caio==henry);
+		System.out.println(false);
+		
+	boolean chovendo = true;
+	boolean relampagos = false; 
+	if (chovendo && relampagos){
+	System.out.println("nao sairei");
+	
 	}
+	    else {
+	    System.out.println("irei sair");
+		}
+		boolean macarrao = true;
+		boolean strogonoff = true;
+		if (macarrao || strogonoff){
+		    System.out.println("ficarei muito feliz");
+		}
+		else {
+		    System.out.println("nao ficarei feliz");
+		}
+}
 }
