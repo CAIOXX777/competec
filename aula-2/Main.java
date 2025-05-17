331cdcea0f0a8b9961161733bc7cd7565ab5f9a7
CAIOXX <104595124+CAIOXX777@users.noreply.github.com>
2025-05-17 11:30:47 -0300

    aula 02.java

--- aula-2/Main.java
+++ aula-2/Main.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
-	}
+	Scanner entrada = new Scanner (System.in);
+	double hamburguer = 19.99;
+	double batata = 7.99;
+	double coca = 5.00;
+	double milkshake = 11.00;
+	
+	
+	
+	double carrinho = 40.00;
+	double carteira = 40.00;
+	
+
+	
+	if(carrinho < carteira){
+	    System.out.println("Compra bem sucedida!");
+	    System.out.println("Sobrou Dinheiro!");
+	} else if(carrinho == carteira){
+	    System.out.println("Compra bem sucedida");
+	    System.out.println("zerei!");}
+	    else{
+	        System.out.println("naum vai cumer");
+	    }
 }
+}	
+
